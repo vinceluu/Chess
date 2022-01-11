d6696e6f9218248d559164c03421b03cddd1ca85
Paloma Sanchez <palomasanchez@Palomas-Air.attlocal.net>
2022-01-10 21:09:37 -0600

    PlayerTurn

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,11 +1,13 @@
 package com.company;
-
+import java.util.Scanner;
 public class Board {
 
     final public Integer ROW_SIZE = 8;
     final public Integer COL_SIZE = 8;
     Tile[][] my_board = new Tile[ROW_SIZE][COL_SIZE];
     public Boolean isGameOver = false;
+    public Boolean isGameOver = false;
+    public Boolean playerTurn = true;
 
     public Board(){
         setupBoard();
@@ -25,10 +27,73 @@ public class Board {
         for (int col = 0; col < this.COL_SIZE; col ++){
             my_board[6][col] = new Tile(new Piece("W", "P"));
         }
-        // add queen
+        my_board[7][0] = new Tile(new Piece("W", "R"));
+        my_board[7][1] = new Tile(new Piece("W", "N"));
+        my_board[7][2] = new Tile(new Piece("W", "B"));
+        my_board[7][3] = new Tile(new Piece("W", "Q"));
+        my_board[7][4] = new Tile(new Piece("W", "K"));
+        my_board[7][5] = new Tile(new Piece("W", "B"));
+        my_board[7][6] = new Tile(new Piece("W", "N"));
+        my_board[7][7] = new Tile(new Piece("W", "R"));
+
+        for (int col = 0; col < this.COL_SIZE; col ++){
+            my_board[1][col] = new Tile(new Piece("B", "P"));
+        }
+        my_board[0][0] = new Tile(new Piece("B", "R"));
+        my_board[0][1] = new Tile(new Piece("B", "N"));
+        my_board[0][2] = new Tile(new Piece("B", "B"));
+        my_board[0][3] = new Tile(new Piece("B", "Q"));
+        my_board[0][4] = new Tile(new Piece("B", "K"));
+        my_board[0][5] = new Tile(new Piece("B", "B"));
+        my_board[0][6] = new Tile(new Piece("B", "N"));
+        my_board[0][7] = new Tile(new Piece("B", "R"));
+
 
     }
+    public void startGame(){
+        Scanner playerInput = new Scanner(System.in);
 
+        while (this.isGameOver == false && this.playerTurn == true) {
+            // White's turn while loop
+            System.out.println("White's turn.");
+            System.out.println("Please enter current piece row: ");
+            String currWhitePieceRow = playerInput.nextLine();
+            System.out.println("Please enter current piece column: ");
+            String currWhitePieceColumn = playerInput.nextLine();
+            System.out.println("Please enter which row to move the piece: ");
+            String moveWhitePieceRow = playerInput.nextLine();
+            System.out.println("Please enter which column to move the piece: ");
+            String moveWhitePieceColumn = playerInput.nextLine();
+            setPlayerTurn(false);
+            while(this.isGameOver == false && this.playerTurn == false){
+                // Black's turn while loop
+                System.out.println("Black's turn.");
+                System.out.println("Please enter current piece row: ");
+                String currBlackPieceRow = playerInput.nextLine();
+                System.out.println("Please enter current piece column: ");
+                String currBlackPieceColumn = playerInput.nextLine();
+                System.out.println("Please enter which row to move the piece: ");
+                String moveBlackPieceRow = playerInput.nextLine();
+                System.out.println("Please enter which column to move the piece: ");
+                String moveBlackPieceColumn = playerInput.nextLine();
+                setPlayerTurn(true);
+                if (isGameOver){
+                    break;
+                }
+            }
+            if (isGameOver){
+                break;
+            }
+        }
+
+    }
+    public void setPlayerTurn(Boolean newPlayerTurn){
+        this.playerTurn = newPlayerTurn;
+    }
+
+    public void setIsGameOver(Boolean newIsGameOver){
+        this.isGameOver = newIsGameOver;
+    }
     public void displayLines(){
         System.out.println();
         System.out.print("  ");
--- src/com/company/Tile.java
+++ src/com/company/Tile.java
@@ -1,5 +1,5 @@
 package com.company;
-
+import java.util.Scanner;
 public class Tile {
 
     Piece curr_piece;
